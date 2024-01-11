f3338586fc2e1d60fe9124c77ce9bcb57b778b06
IlyaGlaz <glazterix@gmail.com>
2024-01-11 22:17:24 +0300

    init

---
+++ src/lesson1/Primitives.java
@@ -0,0 +1,20 @@
+package lesson1;
+
+public class Primitives {
+
+    public static void main(String[] args) {
+        System.out.println(22); // int
+
+        System.out.println(22L); // long
+
+        System.out.println(0.22); // double
+
+        System.out.println(0.22F); // float
+
+        System.out.println('B'); // char
+
+        System.out.println(false); // boolean
+
+        System.out.println("Hello"); // String
+    }
+}
---
+++ src/lesson1/Variables.java
@@ -0,0 +1,41 @@
+package lesson1;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int numberOne; // объявление
+        numberOne = 22; // инициализация
+
+        boolean isRight = false;
+        short smallNumber = 150;
+        long bigNumber = 50;
+
+        char someChar = 'F';
+
+        int numberTwo = someChar;
+
+        double someNumber = numberTwo;
+
+//        System.out.println(someNumber);
+
+        // + - * / %
+
+        int first = 20;
+        int second = 40;
+
+        int result = first - second;
+//        System.out.println(result);
+
+//        System.out.println(4 % 2);
+
+        // < > >= <= == !=
+
+        int someNumberOne = 44;
+        int someNumberTwo = 55;
+
+        boolean resultOfLogic = 66 > 77;
+//        System.out.println(resultOfLogic);
+
+        System.out.println(44 != 55);
+    }
+}
