debe8446e88951702b2d8b3feb318e37fd309e6c
IlyaGlaz <glazterix@gmail.com>
2024-02-03 22:24:00 +0300

    bankaccount

---
+++ src/lesson7/homework/BankAccount.java
@@ -0,0 +1,19 @@
+package lesson7.homework;
+
+public class BankAccount {
+    double amount;
+
+    public double getAmount() {
+        //TODO: реализуйте метод и удалите todo
+        // верните значение количества денег не счету
+        return 0;
+    }
+
+    public void put(double amountToPut) {
+        amount += amountToPut;
+    }
+
+    public void take(double amountToTake) {
+        amount -= amountToTake;
+    }
+}
